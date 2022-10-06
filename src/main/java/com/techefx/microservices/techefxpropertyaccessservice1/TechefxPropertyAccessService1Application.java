63a944bc047f5680e79e21996dad620de366298c
Chinthala, Sudheer (sc849c) <sc849c@us.att.com>
2022-10-06 12:24:36 +0530

    Initial commit

---
+++ src/main/java/com/techefx/microservices/techefxpropertyaccessservice1/TechefxPropertyAccessService1Application.java
@@ -0,0 +1,13 @@
+package com.techefx.microservices.techefxpropertyaccessservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechefxPropertyAccessService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechefxPropertyAccessService1Application.class, args);
+	}
+
+}
